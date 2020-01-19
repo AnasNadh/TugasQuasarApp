35eba40c8058474ec2fc2466bca4b93f4d6851bb
Muhammad Anas Nadhif <48625332+AnasNadh@users.noreply.github.com>
2020-01-19 20:42:23 +0700

    initial commit

---
+++ MyApp/src/pages/Bantuan.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Bantuan</h5>
+    <p>testttttttttttttttttt</p>
+    <p>testttttttttttttttttt</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ MyApp/src/pages/Daftar.vue
@@ -0,0 +1,64 @@
+<template>
+  <q-page class="bg-grey-3 column">
+    <q-list 
+    class="bg-white"
+    separator
+    bordered>
+      <q-item 
+      v-for="task in tasks"
+      :key="task.title"
+      @click="task.done = !task.done" 
+      :class="{ 'done bg-blue-1' : task.done }"
+      clickable
+      v-ripple>
+        <q-item-section avatar top>
+          <q-checkbox 
+          v-model="task.done"  
+          class="no-pointer-events"
+          color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+            <q-item-section
+            v-if="task.done"
+            side>
+          x
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          title: 'Get pict',
+          done: false
+        },
+        {
+          title: 'Up pict',
+          done: true
+        },
+        {
+          title: 'Load pict',
+          done: false
+        },
+      ]
+    }
+  }
+
+}
+</script>
+
+<style lang="scss">
+  .done {
+    .q-item__label {
+      text-decoration: line-through;
+      color: #bbb;
+    }
+  }
+</style>
